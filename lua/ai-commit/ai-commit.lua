8f533106e9a54d48d9ef28bb132aceaf0eff2d09
unknown <nekit2271@gmail.com>
2025-03-17 14:00:43 +0300

    refactor: move main logic into lua directory

--- lua/ai-commit/ai-commit.lua
+++ lua/ai-commit/ai-commit.lua
@@ -15,8 +15,17 @@ M.generate_commit = function()
   commit_generator.generate_commit(M.config)
 end
 
+M.show_commit_suggestions = function(messages)
+  local has_telescope, _ = pcall(require, "telescope")
+  if not has_telescope then
+    error("This plugin requires nvim-telescope/telescope.nvim")
+  end
+
+  require("telescope").extensions["ai-commit"].commit({ messages = messages })
+end
+
 vim.api.nvim_create_user_command("AICommit", function()
   M.generate_commit()
 end, {})
 
-return M
+return M 
