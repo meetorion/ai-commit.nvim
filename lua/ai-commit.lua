38dfdb45850feae9946f647db29e3fe76375f373
unknown <nekit2271@gmail.com>
2025-03-17 13:41:39 +0300

    refactor: split plugin code into multiple files

---
+++ lua/ai-commit.lua
@@ -0,0 +1,22 @@
+local M = {}
+M.config = {
+  openrouter_api_key = nil,
+  model = "qwen/qwen-2.5-72b-instruct:free",
+  auto_push = false,
+}
+
+local commit_generator = require("commit_generator")
+
+M.setup = function(opts)
+  M.config = vim.tbl_deep_extend("force", M.config, opts or {})
+end
+
+M.generate_commit = function()
+  commit_generator.generate_commit(M.config)
+end
+
+vim.api.nvim_create_user_command("AICommit", function()
+  M.generate_commit()
+end, {})
+
+return M 
--- lua/telescope/_extensions/ai-commit.lua
+++ lua/telescope/_extensions/ai-commit.lua
@@ -10,59 +10,67 @@ local actions = require("telescope.actions")
 local action_state = require("telescope.actions.state")
 local themes = require("telescope.themes")
 
+local create_commit_job = function(selection)
+  local Job = require("plenary.job")
+  return Job:new({
+    command = "git",
+    args = { "commit", "-m", selection[1] },
+    on_exit = function(j, return_val)
+      if return_val == 0 then
+        vim.notify("Commit created successfully!", vim.log.levels.INFO)
+        if require("ai-commit").config.auto_push then
+          push_changes()
+        end
+      else
+        vim.notify("Failed to create commit", vim.log.levels.ERROR)
+      end
+    end,
+  })
+end
+
+local push_changes = function()
+  local Job = require("plenary.job")
+  vim.notify("Pushing changes...", vim.log.levels.INFO)
+  Job:new({
+    command = "git",
+    args = { "push" },
+    on_exit = function(_, push_return_val)
+      if push_return_val == 0 then
+        vim.notify("Changes pushed successfully!", vim.log.levels.INFO)
+      else
+        vim.notify("Failed to push changes", vim.log.levels.ERROR)
+      end
+    end,
+  }):start()
+end
+
+local handle_selection = function(prompt_bufnr)
+  local selection = action_state.get_selected_entry()
+  actions.close(prompt_bufnr)
+  vim.notify("Creating commit...", vim.log.levels.INFO)
+  create_commit_job(selection):start()
+end
+
 local commit_picker = function(opts)
   opts = opts or {}
   if opts.theme == nil then
     opts = themes.get_dropdown(opts)
   end
 
-  pickers
-    .new(opts, {
-      prompt_title = "AI Commit Messages",
-      finder = finders.new_table({
-        results = opts.messages or {},
-      }),
-      previewer = false,
-      sorter = conf.generic_sorter(opts),
-      attach_mappings = function(prompt_bufnr)
-        actions.select_default:replace(function()
-          local selection = action_state.get_selected_entry()
-          actions.close(prompt_bufnr)
-
-          vim.notify("Creating commit...", vim.log.levels.INFO)
-          local Job = require("plenary.job")
-          Job
-            :new({
-              command = "git",
-              args = { "commit", "-m", selection[1] },
-              on_exit = function(j, return_val)
-                if return_val == 0 then
-                  vim.notify("Commit created successfully!", vim.log.levels.INFO)
-                  if require("ai-commit").config.auto_push then
-                    vim.notify("Pushing changes...", vim.log.levels.INFO)
-                    Job:new({
-                      command = "git",
-                      args = { "push" },
-                      on_exit = function(_, push_return_val)
-                        if push_return_val == 0 then
-                          vim.notify("Changes pushed successfully!", vim.log.levels.INFO)
-                        else
-                          vim.notify("Failed to push changes", vim.log.levels.ERROR)
-                        end
-                      end,
-                    }):start()
-                  end
-                else
-                  vim.notify("Failed to create commit", vim.log.levels.ERROR)
-                end
-              end,
-            })
-            :start()
-        end)
-        return true
-      end,
-    })
-    :find()
+  pickers.new(opts, {
+    prompt_title = "AI Commit Messages",
+    finder = finders.new_table({
+      results = opts.messages or {},
+    }),
+    previewer = false,
+    sorter = conf.generic_sorter(opts),
+    attach_mappings = function(prompt_bufnr)
+      actions.select_default:replace(function()
+        handle_selection(prompt_bufnr)
+      end)
+      return true
+    end,
+  }):find()
 end
 
 return telescope.register_extension({
