56bbcd8ea19c02fcb74ed58eb4cb12faa5828c58
Nikita Skryabin <nekit2271@gmail.com>
2025-03-20 12:20:07 +0300

    refactor(ai-commit): improve module imports and function structure

--- lua/telescope/_extensions/ai-commit.lua
+++ lua/telescope/_extensions/ai-commit.lua
@@ -1,64 +1,74 @@
-local has_telescope, telescope = pcall(require, "telescope")
-if not has_telescope then
-  error("This plugin requires nvim-telescope/telescope.nvim")
-end
+local telescope = require("telescope")
 
-local pickers = require("telescope.pickers")
-local finders = require("telescope.finders")
-local conf = require("telescope.config").values
-local actions = require("telescope.actions")
-local action_state = require("telescope.actions.state")
-local themes = require("telescope.themes")
-
-local commit_picker = function(opts)
+local function setup_opts(opts)
+  local themes = require("telescope.themes")
   opts = opts or {}
   if opts.theme == nil then
     opts = themes.get_dropdown(opts)
   end
+  return opts
+end
+
+local function push_changes()
+  local Job = require("plenary.job")
+
+  vim.notify("Pushing changes...", vim.log.levels.INFO)
+  Job:new({
+    command = "git",
+    args = { "push" },
+    on_exit = function(_, return_val)
+      if return_val == 0 then
+        vim.notify("Changes pushed successfully!", vim.log.levels.INFO)
+      else
+        vim.notify("Failed to push changes", vim.log.levels.ERROR)
+      end
+    end,
+  }):start()
+end
+
+local function commit_changes(message)
+  local Job = require("plenary.job")
+
+  Job:new({
+    command = "git",
+    args = { "commit", "-m", message },
+    on_exit = function(_, return_val)
+      if return_val == 0 then
+        vim.notify("Commit created successfully!", vim.log.levels.INFO)
+        if require("ai-commit").config.auto_push then
+          push_changes()
+        end
+      else
+        vim.notify("Failed to create commit", vim.log.levels.ERROR)
+      end
+    end,
+  }):start()
+end
+
+local function create_commit_picker(opts)
+  local pickers = require("telescope.pickers")
+  local finders = require("telescope.finders")
+  local conf = require("telescope.config").values
+  local actions = require("telescope.actions")
+  local action_state = require("telescope.actions.state")
+
+  opts = setup_opts(opts)
 
   pickers
     .new(opts, {
       prompt_title = "AI Commit Messages",
-      finder = finders.new_table({
-        results = opts.messages or {},
-      }),
+      finder = finders.new_table({ results = opts.messages or {} }),
       previewer = false,
       sorter = conf.generic_sorter(opts),
       attach_mappings = function(prompt_bufnr)
         actions.select_default:replace(function()
           local selection = action_state.get_selected_entry()
           actions.close(prompt_bufnr)
-
-          vim.notify("Creating commit...", vim.log.levels.INFO)
-          local Job = require("plenary.job")
-          Job
-            :new({
-              command = "git",
-              args = { "commit", "-m", selection[1] },
-              on_exit = function(j, return_val)
-                if return_val == 0 then
-                  vim.notify("Commit created successfully!", vim.log.levels.INFO)
-
-                  if require("ai-commit").config.auto_push then
-                    vim.notify("Pushing changes...", vim.log.levels.INFO)
-                    Job:new({
-                      command = "git",
-                      args = { "push" },
-                      on_exit = function(_, push_return_val)
-                        if push_return_val == 0 then
-                          vim.notify("Changes pushed successfully!", vim.log.levels.INFO)
-                        else
-                          vim.notify("Failed to push changes", vim.log.levels.ERROR)
-                        end
-                      end,
-                    }):start()
-                  end
-                else
-                  vim.notify("Failed to create commit", vim.log.levels.ERROR)
-                end
-              end,
-            })
-            :start()
+          if selection and selection[1] then
+            commit_changes(selection[1])
+          else
+            vim.notify("No commit message selected", vim.log.levels.WARN)
+          end
         end)
         return true
       end,
@@ -67,7 +77,5 @@ local commit_picker = function(opts)
 end
 
 return telescope.register_extension({
-  exports = {
-    commit = commit_picker,
-  },
+  exports = { commit = create_commit_picker },
 })
